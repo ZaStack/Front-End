c774108d0c9b49df15fa7489e73f59638eca6242
Martinezm87 <Martinezm0087@gmail.com>
2020-02-05 18:20:53 -0500

    CRUD mostly done

--- src/App.js
+++ src/App.js
@@ -1,26 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import { BrowserRouter, Route, Link } from 'react-router-dom'
+import "./App.css";
+import UserTable from './tables/UserTable'
+import React, { useState } from 'react'
+import AddUserForm from './forms/AddUserForm.js'
 
-function App() {
+const App = () => {
+  const usersData = [
+    { id: 1, name: 'D&D night', username: 'Dragon Pug, FL', height: 'Firday Night 9pm' },
+    { id: 2, name: 'Movie Night', username: 'My house, CA', height: 'Saturday Night 8pm' },
+    { id: 3, name: 'Taco Tuesday', username: 'TacoPlace, TX', height: 'Tuesday 1:30pm' },
+  ]
+
+  const [users, setUsers] = useState(usersData)
+
+  const addUser = user => {
+    user.id = users.length + 1
+    setUsers([...users, user])
+  }
+
+  const deleteUser = id => {
+    setUsers(users.filter(user => user.id !== id))
+    
+  }
+  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1>BlockClubCalendar</h1>
+      <div className="flex-row">
+        <div className="flex-large">
+          <h2>Add Event</h2>
+          <AddUserForm addUser={addUser} />
+        </div>
+        <div className="flex-large">
+          <h2>View Events</h2>
+          <UserTable users={users} deleteUser={deleteUser} />
+        </div>
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
